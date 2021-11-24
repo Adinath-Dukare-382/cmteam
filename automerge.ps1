762053d0f23a5141329a6037d5a20a26a85e9b6c
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2021-11-24 17:57:00 +0530

    Update automerge.ps1

--- automerge.ps1
+++ automerge.ps1
@@ -61,9 +61,7 @@ param(
 
 curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/branches
 
-curl \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/$Owner/$Repo
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo
   
 curl -X DELETE -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/automated-security-fixes
 curl -X DELETE -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/feature_enable
