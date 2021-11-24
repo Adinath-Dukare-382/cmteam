a416e246f27de5ce2e1474e279d2c995c3ae0949
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2021-11-24 16:56:35 +0530

    Update automerge.ps1

--- automerge.ps1
+++ automerge.ps1
@@ -1,82 +1,90 @@
+
 param(
   [parameter(Mandatory=$True)] 
   [string]$Owner,
   [parameter(Mandatory=$True)] 
-  [string]$Repo,
-  [parameter(Mandatory=$True)] 
-  [string]$FromBranch,
-  [parameter(Mandatory=$True)] 
-  [string]$ToBranch,
-  [parameter(Mandatory=$True)] 
-  [string]$PullRequestTitle,
-  [parameter(Mandatory=$True)] 
-  [string]$Reviewers
+  [string]$Repo
+#   [parameter(Mandatory=$True)] 
+#   [string]$FromBranch,
+#   [parameter(Mandatory=$True)] 
+#   [string]$ToBranch,
+#   [parameter(Mandatory=$True)] 
+#   [string]$PullRequestTitle,
+#   [parameter(Mandatory=$True)] 
+#   [string]$Reviewers
 )
 
-#Reference 
-# https://docs.github.com/en/rest/reference/pulls
+# #Reference 
+# # https://docs.github.com/en/rest/reference/pulls
 
-$ReviewersArray = @($Reviewers)
-#################################
-#$Token = 'Add the token here if running manually and comment below line'   https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
- $Token = $env:GithubToken_ENV_VAR
-#################################
+# $ReviewersArray = @($Reviewers)
+# #################################
+# #$Token = 'Add the token here if running manually and comment below line'   https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
+#  $Token = $env:GithubToken_ENV_VAR
+# #################################
 
-$base64token = [System.Convert]::ToBase64String([char[]]$Token);
+# $base64token = [System.Convert]::ToBase64String([char[]]$Token);
 
-$Headers = @{
-       Authorization = 'Basic {0}' -f $base64token;
-    };
-
-
-$PullRequestBody = @{
-      head = $FromBranch; # from
-      base = $ToBranch; # to 
-      title = $PullRequestTitle; #title of PR
-
-} | ConvertTo-Json;
+# $Headers = @{
+#        Authorization = 'Basic {0}' -f $base64token;
+#     };
 
 
-$MergeBody = @{
-      commit_title = $PullRequestTitle; 
-} | ConvertTo-Json;
+# $PullRequestBody = @{
+#       head = $FromBranch; # from
+#       base = $ToBranch; # to 
+#       title = $PullRequestTitle; #title of PR
+
+# } | ConvertTo-Json;
 
 
-$ReviewerNamesBody = @{   
-    team_reviewers = $ReviewersArray;
-} | ConvertTo-Json;
+# $MergeBody = @{
+#       commit_title = $PullRequestTitle; 
+# } | ConvertTo-Json;
+
+
+# $ReviewerNamesBody = @{   
+#     team_reviewers = $ReviewersArray;
+# } | ConvertTo-Json;
 
 
 
 
-#create pull request
-#$uri = "https://api.github.com/repos/$Owner/$Repo/pulls"
-$uri = "https://github.com/$Owner/$Repo/pulls"
-$createpullrequest = Invoke-RestMethod -Headers $Headers -uri  $uri -Body $PullRequestBody -Method Post
-Start-Sleep -Seconds 10
-#get pull request details
-$uri = $createpullrequest.url
-$PullRequestDetails = Invoke-RestMethod -Headers $Headers -uri $uri
+# #create pull request
+# $uri = "https://api.github.com/repos/$Owner/$Repo/pulls"
+# $uri = "https://github.com/$Owner/$Repo/pulls"
+# $createpullrequest = Invoke-RestMethod -Headers $Headers -uri  $uri -Body $PullRequestBody -Method Post
+# Start-Sleep -Seconds 10
+# #get pull request details
+# $uri = $createpullrequest.url
+# $PullRequestDetails = Invoke-RestMethod -Headers $Headers -uri $uri
 
 
-#check if the code is mergeable (there are no conflicts)
-if(($PullRequestDetails.mergeable -eq "True"))
-{ 
-   #merge Pullrequest
-   $uri = $createpullrequest.url + "/merge"
-   $mergedPR = Invoke-RestMethod -Headers $Headers -uri  $uri -Method PUT -Body $MergeBody
-   $mergedPR
+curl \
+  -X DELETE \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/$Owner/$Repo
 
-}
-else
-{
-#add reviewers
-$uri = $createpullrequest.url + "/requested_reviewers"
-$requestreview = Invoke-RestMethod -Headers $Headers -uri $uri -Body $ReviewerNamesBody -Method Post
-$PullRequestDetails
-Write-Error "There are conflicts and needs to be resolved"
 
-}
 
-curl -s -X DELETE -u ['Adinath-Dukare-382']:[$Token] https://api.github.com/repos/[$Owner]/[$Repo]/git/refs/heads/feature_main
+# #check if the code is mergeable (there are no conflicts)
+# if(($PullRequestDetails.mergeable -eq "True"))
+# { 
+#    #merge Pullrequest
+#    $uri = $createpullrequest.url + "/merge"
+#    $mergedPR = Invoke-RestMethod -Headers $Headers -uri  $uri -Method PUT -Body $MergeBody
+#    $mergedPR
+
+# }
+# else
+# {
+# #add reviewers
+# $uri = $createpullrequest.url + "/requested_reviewers"
+# $requestreview = Invoke-RestMethod -Headers $Headers -uri $uri -Body $ReviewerNamesBody -Method Post
+# $PullRequestDetails
+# Write-Error "There are conflicts and needs to be resolved"
+
+# }
+
+# curl -s -X DELETE -u ['Adinath-Dukare-382']:[$Token] https://api.github.com/repos/[$Owner]/[$Repo]/git/refs/heads/feature_main
 
