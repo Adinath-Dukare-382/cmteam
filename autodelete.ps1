74e86fb1e1352fe0dfd049f109f7a0c2a6dba07b
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2021-11-25 16:44:05 +0530

    Update autodelete.ps1

--- autodelete.ps1
+++ autodelete.ps1
@@ -14,7 +14,8 @@ curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/Ad
 
 curl -X GET -u Adinath-Dukare-382:$TokenGithub https://api.github.com/repos/Adinath-Dukare-382/githubpoc/commits/b20b89382683ceaee0e84021403248031bd5be3b/pulls
 
-
+$prinfo = curl.exe -X GET -u Adinath-Dukare-382:$TokenGithub https://api.github.com/repos/Adinath-Dukare-382/githubpoc/pulls/3 | ConvertFrom-Json
+$prinfo
 
 write-host ---------------------------------------------------------------------------
 
