7367cc762c20a4223b99363f4ab35ef350419469
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2021-11-25 12:45:52 +0530

    Update autodelete.ps1

--- autodelete.ps1
+++ autodelete.ps1
@@ -33,3 +33,11 @@ $webRequest = Invoke-RestMethod -URI "http://api.open-notify.org/astros.json"
 
 $createpullrequest = Invoke-RestMethod -Headers $Headers
 
+
+#get branches
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/branches
+#curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/branches/main
+#curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/branches/feature_enable
+#curl -X DELETE -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/git/refs/heads/main
+#curl -X DELETE -u Adinath-Dukare-382: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 https://api.github.com/repos/$Owner/$Repo
+#curl -X DELETE -u Adinath-Dukare-382:$TokenGithub https://api.github.com/repos/$Owner/$Repo/git/refs/heads/feature_enable
