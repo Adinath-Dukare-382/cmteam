871b281c3b3f294f428e98b335d024f641d73b77
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2021-11-25 16:10:44 +0530

    Update autodelete.ps1

--- autodelete.ps1
+++ autodelete.ps1
@@ -6,39 +6,19 @@ param(
   [string]$Repo,
   [parameter(Mandatory=$True)] 
   [string]$TokenGithub
-#   [parameter(Mandatory=$True)] 
-#   [string]$FromBranch,
-#   [parameter(Mandatory=$True)] 
-#   [string]$ToBranch,
-#   [parameter(Mandatory=$True)] 
-#   [string]$PullRequestTitle,
-#   [parameter(Mandatory=$True)] 
-#   [string]$Reviewers
 )
 
- #$Token = $env:GithubToken_ENV_VAR
-
  $Token = $env:GithubToken_ENV_VAR
-#################################
-
-$base64token = [System.Convert]::ToBase64String([char[]]$Token);
-
-$Headers = @{
-       Authorization = 'Basic {0}' -f $base64token;
-    };
-
-write-host $Headers
-
-write-host ---------------------------------------------------------------------------
-
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/pulls/33
 
 
 write-host ---------------------------------------------------------------------------
 
-#$createpullrequest = Invoke-RestMethod -Headers $Headers
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/pulls/25
 
-curl -s -H "Authorization: token $TokenGithub" -H "Accept: application/vnd.github.VERSION.sha" "https://api.github.com/repos/$Owner/$Repo/commits/main"
+
+write-host ---------------------------------------------------------------------------
+
+#curl -s -H "Authorization: token $TokenGithub" -H "Accept: application/vnd.github.VERSION.sha" "https://api.github.com/repos/$Owner/$Repo/commits/main"
 
 write-host ---------------------------------------------------------------------------
 
