7bcd9471e075896102bc5aebf12a908b665fd728
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2021-11-25 12:36:35 +0530

    Update automerge.ps1

--- automerge.ps1
+++ automerge.ps1
@@ -16,17 +16,26 @@ param(
 #   [string]$Reviewers
 )
 
+ #$Token = $env:GithubToken_ENV_VAR
+
  $Token = $env:GithubToken_ENV_VAR
+#################################
 
+$base64token = [System.Convert]::ToBase64String([char[]]$Token);
 
+$Headers = @{
+       Authorization = 'Basic {0}' -f $base64token;
+    };
+
+write-host $Headers
 
 #get branches
 curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/branches
 #curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/branches/main
 #curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/branches/feature_enable
-curl -X DELETE -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/git/refs/heads/main
+#curl -X DELETE -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/git/refs/heads/main
 #curl -X DELETE -u Adinath-Dukare-382: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 https://api.github.com/repos/$Owner/$Repo
-curl -X DELETE -u Adinath-Dukare-382:$TokenGithub https://api.github.com/repos/$Owner/$Repo/git/refs/heads/feature_enable
+#curl -X DELETE -u Adinath-Dukare-382:$TokenGithub https://api.github.com/repos/$Owner/$Repo/git/refs/heads/feature_enable
 
 #getrepo
 #curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo
