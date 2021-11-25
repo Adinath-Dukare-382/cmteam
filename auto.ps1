d7d06cdd01d7ace874d08e0bc0d36f8b748196e0
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2021-11-25 18:00:28 +0530

    Update auto.ps1

--- auto.ps1
+++ auto.ps1
@@ -10,4 +10,11 @@ param(
 )
 write-Host $Version
 
-curl -X GET -u Adinath-Dukare-382:$TokenGithub https://api.github.com/repos/Adinath-Dukare-382/cmteam/commits/$Version/pulls
+$PullRequest = curl -X GET -u Adinath-Dukare-382:$TokenGithub https://api.github.com/repos/Adinath-Dukare-382/cmteam/commits/$Version/pulls
+$prurl = $PullRequest.url
+
+$prinfo = curl -X GET -u Adinath-Dukare-382:$TokenGithub $prurl | ConvertFrom-Json
+$branch = $prinfo.head.ref
+$branch
+
+curl -X DELETE -u Adinath-Dukare-382:$TokenGithub https://api.github.com/repos/Adinath-Dukare-382/githubpoc/git/refs/heads/$branch
