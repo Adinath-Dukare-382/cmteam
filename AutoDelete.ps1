88a6238eb07e88094b44089c7437c9bd50198643
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2021-11-26 15:22:14 +0530

    Update AutoDelete.ps1

--- AutoDelete.ps1
+++ AutoDelete.ps1
@@ -9,8 +9,20 @@ param(
 
 $TokenGithub = $env:GithubToken_ENV_VAR
 
+$base64token = [System.Convert]::ToBase64String([char[]]$TokenGithub);
+
+$Headers = @{
+       Authorization = 'Basic {0}' -f $base64token;
+    };
+
+$urlbody = https://api.github.com/repos/$Owner/$Repo/commits/$Version/pulls
+$PullRequest1 = Invoke-RestMethod -Headers $Headers -uri $urlbody -Method Get
+$PullRequest1
+
 $PullRequest = curl -X GET -u ${Owner}:$TokenGithub https://api.github.com/repos/$Owner/$Repo/commits/$Version/pulls | Convertfrom-Json
 $prurl = $PullRequest.url
+
+
 
 $prinfo = curl -X GET -u ${Owner}:$TokenGithub $prurl | ConvertFrom-Json
 $branch = $prinfo.head.ref
