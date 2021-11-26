e432ac88778bdcdff4df6d736ec3fc4a963ceb8a
Adinath-Dukare-382 <adinath.dukare@allscripts.com>
2021-11-26 16:05:18 +0530

    added

--- AutoDelete.ps1
+++ AutoDelete.ps1
@@ -17,30 +17,46 @@ $Headers = @{
 
 
 $urlCommitId = "https://api.github.com/repos/$Owner/$Repo/commits/$Version/pulls"
-$PullRequest = Invoke-RestMethod -Headers $Headers -uri $urlCommitId -Method Get
-$PullRequest
-#$PullRequest = curl -X GET -u ${Owner}:$TokenGithub https://api.github.com/repos/$Owner/$Repo/commits/$Version/pulls | Convertfrom-Json
+$PullRequest1 = Invoke-RestMethod -Headers $Headers -uri $urlCommitId -Method Get
 
+$prUrl = "$PullRequest.url"
 
-
-$PullRequest = curl -X GET -u ${Owner}:$TokenGithub https://api.github.com/repos/$Owner/$Repo/commits/$Version/pulls | Convertfrom-Json
-$prUrl = $PullRequest.url
-
-
-
-$prinfo = curl -X GET -u ${Owner}:$TokenGithub $prurl | ConvertFrom-Json
+$prinfo = Invoke-RestMethod -Headers $Headers -uri $prUrl -Method Get
 $branch = $prinfo.head.ref
 
-$branchInfo = curl -X GET -u ${Owner}:$TokenGithub https://api.github.com/repos/$Owner/$Repo/git/refs/heads/$branch | ConvertFrom-Json
-if($branchInfo.message)
-  {
-      Write-Host "Already branch deleted or no longer exists...!"
-  }
-else
-  {
-      curl -s -X DELETE -u $(user):$(GithubToken) https://api.github.com/repos/$Owner/$Repo/git/refs/heads/$branch
-      Write-Host $branchtobedeleted" Branch Deleted..!" 
-  }
+$branchUrl = "https://api.github.com/repos/$Owner/$Repo/git/refs/heads/$branch"
+$branchInfo = Invoke-RestMethod -Headers $Headers -uri $branchUrl -Method Get
+
+if($branchInfo.message){
+    Write-Host $branch"branch either be deleted or no longer exists...!"
+}
+else{
+    Invoke-RestMethod -Headers $Headers -uri $branchUrl -Method Delete
+    Write-Host $branch" Branch Deleted..!"
+}
+     
+
+
+#$PullRequest = curl -X GET -u ${Owner}:$TokenGithub https://api.github.com/repos/$Owner/$Repo/commits/$Version/pulls | Convertfrom-Json
+#$prUrl = $PullRequest.url
+
+
+
+#$prinfo = curl -X GET -u ${Owner}:$TokenGithub $prurl | ConvertFrom-Json
+#$branch = $prinfo.head.ref
+
+
+
+#$branchInfo = curl -X GET -u ${Owner}:$TokenGithub https://api.github.com/repos/$Owner/$Repo/git/refs/heads/$branch | ConvertFrom-Json
+#if($branchInfo.message)
+#  {
+#      Write-Host "Already branch deleted or no longer exists...!"
+#  }
+#else
+#  {
+#      curl -s -X DELETE -u $(user):$(GithubToken) https://api.github.com/repos/$Owner/$Repo/git/refs/heads/$branch
+#      Write-Host $branchtobedeleted" Branch Deleted..!" 
+#  }
   
 # write-Host 'Deleting branch...'
 # curl -X DELETE -u ${Owner}:$TokenGithub https://api.github.com/repos/$Owner/$Repo/git/refs/heads/$branch
