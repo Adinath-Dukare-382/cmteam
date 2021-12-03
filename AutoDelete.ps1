4488dd4ca643b1cd634937477e3001a616fe9b9c
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2021-12-03 10:18:43 +0530

    Update AutoDelete.ps1

--- AutoDelete.ps1
+++ AutoDelete.ps1
@@ -18,8 +18,8 @@ $Headers = @{
 $deletebody  = @{
 #   "title"="delete";
 #   state= "closed"
-\""title\"": \""open\"";
-  };
+\""title\"" = \""open\"";
+  } | ConvertTo-Json;
 
 $uri = "https://api.github.com/Adinath-Dukare-382/cmteam/pulls/103"
 $closedpr = Invoke-RestMethod -Headers $Headers -uri $uri -Body $deletebody -Method Patch 
