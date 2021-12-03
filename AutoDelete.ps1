59d62ff5b5232721f7f94d256b2aea3f5f4bab2f
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2021-12-03 12:46:13 +0530

    Update AutoDelete.ps1

--- AutoDelete.ps1
+++ AutoDelete.ps1
@@ -75,7 +75,7 @@ try {
     }
     catch
     {
-        $getpr = curl.exe -X GET -u Adinath-Dukare-382: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 https://api.github.com/repos/Adinath-dukare-382/cmteam/pulls?searchCriteria.sourceRefName=$branchToBeDeleted ConvertFrom-Json
+        $getpr = curl.exe -X GET -u Adinath-Dukare-382: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 https://api.github.com/repos/Adinath-dukare-382/cmteam/pulls?searchCriteria.sourceRefName=$branchToBeDeleted | ConvertFrom-Json
         $already = $getpr.state
 
         if($already -eq 'open'){
