02a4f5b6c10dc2ff46cea65cb709fe9296dbb9c8
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2022-04-12 19:17:29 +0530

    Update testing_xcopy.ps1

--- testing_xcopy.ps1
+++ testing_xcopy.ps1
@@ -1,7 +1,14 @@
+[CmdletBinding()]
+  param (
+    [parameter(Mandatory=$True)]
+    [string]$branchName
+  )
+  
 $source = "C:\Users\A888382\Desktop\Dustbin\zips\HIMReportDataSync"
 $destination = "C:\Users\A888382\Desktop\Dustbin\zips\COPYFiles\"
 
 write-host $source
 write-host $destination
+write-host $branchName
 
-${{ github.workspace }}\call_testing_xcopy.ps1 -branch "CMTEAM"
+call_testing_xcopy.ps1 -branch "CMTEAM"
