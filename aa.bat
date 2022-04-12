1f5d795f157af22ae3549817302591a4b1371a73
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2022-04-12 17:43:38 +0530

    Update aa.bat

--- aa.bat
+++ aa.bat
@@ -8,7 +8,7 @@ set "destination=C:\Users\A888382\Desktop\Dustbin\zips\COPYFiles\"
 echo "%source%appsettings.json"
 
 
-xcopy "%source%appsettings.json"	"%destination%"  /c /d /i /y
+xcopy "%source%appsettings.json" "%destination%"  /c /d /i /y
 xcopy "%source%microsoft.codeanalysis.csharp.dll"	"%destination%"  /c /d /i /y
 xcopy "%source%microsoft.codeanalysis.csharp.dll"	"%destination%" /c /d /i /y 
 xcopy "%source%microsoft.codeanalysis.visualbasic.dll" "%destination%" /c /d /i /y 
