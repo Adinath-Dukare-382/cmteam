f927c5904b184c2291844b84beebd39d9997977f
Adinath-Dukare-382 <adinath.dukare@allscripts.com>
2022-04-13 08:41:52 +0530

    test

--- call_testing_xcopy.ps1
+++ call_testing_xcopy.ps1
@@ -4,5 +4,12 @@
     [string]$branch
   )
 $ad = "Adinath"
+write-host $ad
+$source = "C:\Users\A888382\Desktop\Dustbin\zips\HIMReportDataSync"
+$destination = "C:\Users\A888382\Desktop\Dustbin\zips\COPYFiles\"
+   
+xcopy "$source\appsettings.json" "$destination"  /c /d /i /y
+xcopy "$source\microsoft.codeanalysis.csharp.dll" "$destination"  /c /d /i /y
+xcopy "$source\appsettings.himreport.json"	"$destination"  /c /d /i /y
 
 write-host $branch
